912ccb58a6274e2c426c45fed44ee7a664a35e87
Yifan Gu <20095253@mail.wit.ie>
2024-01-31 04:32:18 +0800

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,47 @@
 import {Friend, Colleague } from './myTypes'
+import { friends,colleagues } from './01-basics'
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}` 
+}
+
+function allOlder(f: Friend[]): string[] {
+    return friends.map(friend => older(friend));
+}
+
+console.log(older(friends[0]))
+console.log(allOlder(friends));
+
+function addColleague(colleaguesArray: Colleague[], name: string, department: string, email: string): void {
+  let highestExtension = 0;
+
+  colleaguesArray.forEach(colleague => {
+      if (colleague.contact.extension > highestExtension) {
+          highestExtension = colleague.contact.extension;
+      }
+  });
+
+  const newColleague: Colleague = {
+      name: name,
+      department: department,
+      contact: {
+          email: email,
+          extension: highestExtension + 1
+      }
+  };
+
+  colleaguesArray.push(newColleague);
+}
+
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+      (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+  }
+  console.log(highestExtension(colleagues.current));
+
+  addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
